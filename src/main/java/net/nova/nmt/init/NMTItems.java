bb938e79e5db788a8e8beff3b26064a1ab8b6515
Starexify <nightbear843@yahoo.com>
2024-11-04 18:10:34 +0100

    1.1.0

--- src/main/java/net/nova/nmt/init/NMTItems.java
+++ src/main/java/net/nova/nmt/init/NMTItems.java
@@ -28,5 +28,5 @@ public class NMTItems {
     // Tear of the Nether
     public static final DeferredItem<Item> TEAR_OF_THE_NETHER = ITEMS.register("tear_of_the_nether", () -> new Item(new Item.Properties()));
     public static final DeferredItem<Item> BLAZING_WITHER_SOUL = ITEMS.register("blazing_wither_soul", () -> new Item(new Item.Properties()));
-    public static final DeferredItem<Item> HEART_OF_THE_HELL = ITEMS.register("blazing_wither_soul", () -> new Item(new Item.Properties()));
+    public static final DeferredItem<Item> HEART_OF_THE_HELL = ITEMS.register("heart_of_the_hell", () -> new Item(new Item.Properties()));
 }
